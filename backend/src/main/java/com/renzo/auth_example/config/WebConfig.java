badb055ea8b333d56e9e7824e3b150851b897bda
rgpq25 <renzopq@hotmail.com>
2025-10-15 18:13:13 -0500

    feat(cors): Added CORS config and initial API

---
+++ backend/src/main/java/com/renzo/auth_example/config/WebConfig.java
@@ -0,0 +1,32 @@
+package com.renzo.auth_example.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfig() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:5173")
+                        .allowedMethods(
+                                HttpMethod.GET.name(),
+                                HttpMethod.POST.name(),
+                                HttpMethod.PUT.name(),
+                                HttpMethod.DELETE.name()
+                        )
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,29 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 function App() {
+	const [message, setMessage] = useState("waiting...");
+
+	useEffect(() => {
+		async function initialRequest() {
+			try {
+				const response = await axios.get(
+					"http://localhost:8080/example"
+				);
+				setMessage(response.data);
+			} catch (error) {
+				console.error("There was an error:", error);
+				setMessage("Error fetching message!");
+			}
+		}
+
+		initialRequest();
+	}, []);
+
 	return (
-		<div className="w-dvw h-dvh flex items-center justify-center">
+		<div className="w-dvw h-dvh flex flex-col items-center justify-center">
 			<h1 className="text-3xl">Java Auth Example</h1>
+			<p>Initial response message: {message}</p>
 		</div>
 	);
 }
