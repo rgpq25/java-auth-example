c1ed20a2e09bcf70ca3b07a904ec46ebc244f579
rgpq25 <renzopq@hotmail.com>
2025-10-15 20:28:17 -0500

    feat(entity): Add User entity with basic fields
    Introduces the User entity extending BaseEntity, including fields for name, lastname, email (unique), and password, with appropriate JPA annotations and standard getters/setters.

---
+++ backend/src/main/java/com/renzo/auth_example/user/User.java
@@ -0,0 +1,73 @@
+package com.renzo.auth_example.user;
+
+import com.renzo.auth_example.common.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+
+import java.util.Objects;
+
+@Entity
+public class User extends BaseEntity {
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String lastname;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    protected User() { super(); }
+
+    public User(String name, String lastname, String email, String password) {
+        this.name = name;
+        this.lastname = lastname;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
