35d7d4d44574151ec3777d73177445632858c564
rgpq25 <renzopq@hotmail.com>
2025-12-09 18:15:36 -0500

    fix(field): reduce gap size

--- frontend/src/pages/login.tsx
+++ frontend/src/pages/login.tsx
@@ -1,11 +1,25 @@
+import { useState } from "react";
 import GoogleIcon from "@/assets/google-icon.svg";
+import AuthLoading from "@/components/auth-loading";
 import { useAuth } from "@/components/auth-provider";
 import { Button } from "@/components/ui/button";
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardHeader,
+	CardTitle,
+} from "@/components/ui/card";
+import {
+	Field,
+	FieldDescription,
+	FieldGroup,
+	FieldLabel,
+	FieldSeparator,
+} from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
-import { Separator } from "@/components/ui/separator";
 import { useMutation } from "@tanstack/react-query";
-import { AlertCircle, Loader2 } from "lucide-react";
-import { useState } from "react";
+import { AlertCircle, GalleryVerticalEnd, Loader2 } from "lucide-react";
 import { Link, Navigate } from "react-router-dom";
 
 type LoginForm = {
@@ -16,7 +30,7 @@ type LoginForm = {
 export function Login() {
 	const { isAuthenticated, isLoading, login } = useAuth();
 
-	const [loginForm, setLoginForm] = useState<LoginForm>({
+	const [form, setForm] = useState<LoginForm>({
 		email: "",
 		password: "",
 	});
@@ -25,125 +39,127 @@ export function Login() {
 		mutationFn: login,
 	});
 
-	async function onSignUpWithGoogle() {
-		return;
-	}
+	const handleChange =
+		(field: keyof LoginForm) =>
+		(event: React.ChangeEvent<HTMLInputElement>) => {
+			setForm((prev) => ({
+				...prev,
+				[field]: event.target.value,
+			}));
+		};
 
-	if (isLoading) return null;
+	const handleLogin = () => {
+		loginMutation.mutate(form);
+	};
+
+	if (isLoading) return <AuthLoading />;
 
 	if (isAuthenticated === true) return <Navigate to="/profile" replace />;
 
 	return (
-		<div className="flex h-dvh">
-			<div className="w-1/2 h-full bg-secondary"></div>
-			<div className="w-1/2 h-full flex">
-				<div className="m-auto flex flex-col gap-4 w-xs">
-					<header>
-						<h1 className="text-2xl font-semibold text-center">
-							Welcome back!
-						</h1>
-						<p className="text-muted-foreground text-center">
-							Sign in to your account
-						</p>
-					</header>
-					<section className="space-y-2">
-						<InputField
-							field="email"
-							placeholder="youremail@hotmail.com"
-							value={loginForm}
-							onChange={setLoginForm}
-						/>
-						<InputField
-							field="password"
-							placeholder="verySecurePassword"
-							value={loginForm}
-							onChange={setLoginForm}
-						/>
-					</section>
-					{loginMutation.error !== null && (
-						<div className="rounded-sm border border-red-500 bg-red-100/80 px-3 py-3 flex flex-row items-center gap-2">
-							<AlertCircle className="size-5 stroke-red-500 stroke-2" />
-							<p className="text-sm text-red-500">
-								{loginMutation.error.message}
-							</p>
-						</div>
-					)}
-					<Button
-						className="flex w-full items-center justify-center"
-						onClick={() => loginMutation.mutate(loginForm)}
-						disabled={loginMutation.isPending}
-					>
-						{loginMutation.isPending ? (
-							<Loader2 className="size-4 animate-spin" />
-						) : null}
-						Login
-					</Button>
-					<section className="flex items-center gap-2">
-						<Separator
-							orientation="horizontal"
-							className="flex-1"
-						/>
-						<span className="shrink-0 text-muted-foreground leading-3">
-							or
-						</span>
-						<Separator
-							orientation="horizontal"
-							className="flex-1"
-						/>
-					</section>
-					<Button
-						variant="outline"
-						className="items-center flex"
-						onClick={onSignUpWithGoogle}
-					>
-						<img
-							src={GoogleIcon}
-							alt="Google Icon"
-							className="size-4"
-						/>
-						Sign in with Google
-					</Button>
-					<section>
-						<p className="text-sm text-center text-muted-foreground">
-							Don't have an account?{" "}
-							<Link
-								className="hover:underline font-medium text-primary cursor-pointer"
-								to={"/register"}
-							>
-								Sign up
-							</Link>
-						</p>
-					</section>
+		<div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+			<div className="flex w-full max-w-sm flex-col gap-6">
+				<a
+					href="#"
+					className="flex items-center gap-2 self-center font-medium"
+				>
+					<div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
+						<GalleryVerticalEnd className="size-4" />
+					</div>
+					Java Auth Example
+				</a>
+				<div className={"flex flex-col gap-6"}>
+					<Card className="gap-4">
+						<CardHeader className="text-center">
+							<CardTitle className="text-xl">
+								Welcome back
+							</CardTitle>
+							<CardDescription>
+								Login with your Google account
+							</CardDescription>
+						</CardHeader>
+						<CardContent>
+							<FieldGroup className="gap-6">
+								<Field>
+									<Button
+										variant="outline"
+										type="button"
+										className="items-center flex"
+									>
+										<img
+											src={GoogleIcon}
+											alt="Google Icon"
+											className="size-4"
+										/>
+										Login with Google
+									</Button>
+								</Field>
+								<FieldSeparator className="*:data-[slot=field-separator-content]:bg-card">
+									Or continue with
+								</FieldSeparator>
+								<Field>
+									<FieldLabel htmlFor="email">
+										Email
+									</FieldLabel>
+									<Input
+										id="email"
+										type="email"
+										placeholder="m@example.com"
+										required
+										value={form.email}
+										onChange={handleChange("email")}
+									/>
+								</Field>
+								<Field>
+									<div className="flex items-center">
+										<FieldLabel htmlFor="password">
+											Password
+										</FieldLabel>
+										<a
+											href="#"
+											className="ml-auto text-sm underline-offset-4 hover:underline"
+										>
+											Forgot your password?
+										</a>
+									</div>
+									<Input
+										id="password"
+										type="password"
+										placeholder="safePassword"
+										required
+										value={form.password}
+										onChange={handleChange("password")}
+									/>
+								</Field>
+								{loginMutation.error && (
+									<div className="rounded-sm border border-red-500 bg-red-100/80 px-3 py-3 flex flex-row items-center gap-2">
+										<AlertCircle className="size-5 stroke-red-500 stroke-2" />
+										<p className="text-sm text-red-500">
+											{loginMutation.error.message}
+										</p>
+									</div>
+								)}
+								<Field className="flex flex-col gap-3">
+									<Button
+										type="button"
+										onClick={handleLogin}
+										disabled={loginMutation.isPending}
+									>
+										{loginMutation.isPending ? (
+											<Loader2 className="size-4 animate-spin" />
+										) : null}
+										Login
+									</Button>
+									<FieldDescription className="text-center">
+										Don&apos;t have an account?{" "}
+										<Link to={"/register"}>Sign up</Link>
+									</FieldDescription>
+								</Field>
+							</FieldGroup>
+						</CardContent>
+					</Card>
 				</div>
 			</div>
-		</div>
-	);
-}
-
-function InputField({
-	field,
-	placeholder,
-	value,
-	onChange,
-}: {
-	field: keyof LoginForm;
-	placeholder: string;
-	value: LoginForm;
-	onChange: (val: LoginForm) => void;
-}) {
-	return (
-		<div className="flex flex-col gap-1">
-			<p className="text-sm font-semibold capitalize">{field}</p>
-			<Input
-				placeholder={placeholder}
-				value={value[field]}
-				onChange={(e) =>
-					onChange({
-						...value,
-						[field]: e.target.value,
-					})
-				}
-			/>
 		</div>
 	);
 }
