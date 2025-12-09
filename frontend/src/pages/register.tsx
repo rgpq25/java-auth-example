5e6722132ec55aa4c01645f77d2cbbfe5aae3c7b
rgpq25 <renzopq@hotmail.com>
2025-12-09 18:43:30 -0500

    feat(auth): standarized design for auth flow

--- frontend/src/pages/register.tsx
+++ frontend/src/pages/register.tsx
@@ -1,11 +1,26 @@
 import GoogleIcon from "@/assets/google-icon.svg";
+import AuthLoading from "@/components/auth-loading";
 import { useAuth } from "@/components/auth-provider";
 import { Button } from "@/components/ui/button";
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardHeader,
+	CardTitle,
+} from "@/components/ui/card";
+import {
+	Field,
+	FieldDescription,
+	FieldGroup,
+	FieldLabel,
+	FieldSeparator,
+} from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
-import { Separator } from "@/components/ui/separator";
-import { Loader2 } from "lucide-react";
+import { useMutation } from "@tanstack/react-query";
+import { AlertCircle, GalleryVerticalEnd, Loader2 } from "lucide-react";
 import { useState } from "react";
-import { Link, Navigate, useNavigate } from "react-router-dom";
+import { Link, Navigate } from "react-router-dom";
 
 type RegisterForm = {
 	name: string;
@@ -15,148 +30,144 @@ type RegisterForm = {
 
 export function Register() {
 	const { isAuthenticated, isLoading, register } = useAuth();
-	const navigate = useNavigate();
 
-	const [registerForm, setRegisterForm] = useState<RegisterForm>({
+	const [form, setForm] = useState<RegisterForm>({
 		name: "",
 		email: "",
 		password: "",
 	});
-	const [isFormLoading, setIsFormLoading] = useState(false);
 
-	async function onRegister() {
-		try {
-			setIsFormLoading(true);
+	const registerMutation = useMutation({
+		mutationFn: register,
+	});
 
-			await register({
-				name: registerForm.name,
-				email: registerForm.email,
-				password: registerForm.password,
-			});
+	const handleChange =
+		(field: keyof RegisterForm) =>
+		(event: React.ChangeEvent<HTMLInputElement>) => {
+			setForm((prev) => ({
+				...prev,
+				[field]: event.target.value,
+			}));
+		};
 
-			navigate("/profile", { replace: true });
-		} finally {
-			setIsFormLoading(false);
-		}
-	}
+	const handleRegister = () => {
+		registerMutation.mutate(form);
+	};
 
-	async function onSignUpWithGoogle() {
-		return;
-	}
-
-	if (isLoading) return null;
+	if (isLoading) return <AuthLoading />;
 
 	if (isAuthenticated === true) return <Navigate to="/profile" replace />;
 
 	return (
-		<div className="flex h-dvh">
-			<div className="w-1/2 h-full bg-secondary"></div>
-			<div className="w-1/2 h-full flex">
-				<div className="m-auto flex flex-col gap-4 w-xs">
-					<header>
-						<h1 className="text-2xl font-semibold text-center">
-							Create an account
-						</h1>
-						<p className="text-muted-foreground text-center">
-							Start your journey by registering your account
-						</p>
-					</header>
-					<section className="space-y-2">
-						<InputField
-							field="name"
-							placeholder="Jhon"
-							value={registerForm}
-							onChange={setRegisterForm}
-						/>
-						<InputField
-							field="email"
-							placeholder="youremail@hotmail.com"
-							value={registerForm}
-							onChange={setRegisterForm}
-						/>
-						<InputField
-							field="password"
-							placeholder="verySecurePassword"
-							value={registerForm}
-							onChange={setRegisterForm}
-						/>
-					</section>
-					<Button
-						className="flex w-full items-center justify-center"
-						onClick={onRegister}
-						disabled={isFormLoading}
-					>
-						{isFormLoading ? (
-							<Loader2 className="size-4 animate-spin" />
-						) : null}
-						Register
-					</Button>
-					<section className="flex items-center gap-2">
-						<Separator
-							orientation="horizontal"
-							className="flex-1"
-						/>
-						<span className="shrink-0 text-muted-foreground leading-3">
-							or
-						</span>
-						<Separator
-							orientation="horizontal"
-							className="flex-1"
-						/>
-					</section>
-					<Button
-						variant="outline"
-						className="items-center flex"
-						onClick={onSignUpWithGoogle}
-					>
-						<img
-							src={GoogleIcon}
-							alt="Google Icon"
-							className="size-4"
-						/>
-						Sign up with google
-					</Button>
-					<section>
-						<p className="text-sm text-center text-muted-foreground">
-							Already have an account?{" "}
-							<Link
-								className="hover:underline font-medium text-primary cursor-pointer"
-								to={"/login"}
-							>
-								Sign in
-							</Link>
-						</p>
-					</section>
+		<div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+			<div className="flex w-full max-w-sm flex-col gap-6">
+				<a
+					href="#"
+					className="flex items-center gap-2 self-center font-medium"
+				>
+					<div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
+						<GalleryVerticalEnd className="size-4" />
+					</div>
+					Java Auth Example
+				</a>
+				<div className={"flex flex-col gap-6"}>
+					<Card className="gap-4">
+						<CardHeader className="text-center">
+							<CardTitle className="text-xl">
+								Create an account
+							</CardTitle>
+							<CardDescription>
+								Start your journey by registering your account
+							</CardDescription>
+						</CardHeader>
+						<CardContent>
+							<FieldGroup className="gap-6">
+								<Field>
+									<Button
+										variant="outline"
+										type="button"
+										className="items-center flex"
+										disabled={registerMutation.isPending}
+									>
+										<img
+											src={GoogleIcon}
+											alt="Google Icon"
+											className="size-4"
+										/>
+										Login with Google
+									</Button>
+								</Field>
+								<FieldSeparator className="*:data-[slot=field-separator-content]:bg-card">
+									Or continue with
+								</FieldSeparator>
+								<Field>
+									<FieldLabel htmlFor="email">
+										Name
+									</FieldLabel>
+									<Input
+										id="name"
+										type="text"
+										placeholder="Jhon Doe"
+										required
+										value={form.name}
+										onChange={handleChange("name")}
+									/>
+								</Field>
+								<Field>
+									<FieldLabel htmlFor="email">
+										Email
+									</FieldLabel>
+									<Input
+										id="email"
+										type="email"
+										placeholder="email@example.com"
+										required
+										value={form.email}
+										onChange={handleChange("email")}
+									/>
+								</Field>
+								<Field>
+									<FieldLabel htmlFor="password">
+										Password
+									</FieldLabel>
+									<Input
+										id="password"
+										type="password"
+										placeholder="safePassword"
+										required
+										value={form.password}
+										onChange={handleChange("password")}
+									/>
+								</Field>
+								{registerMutation.error && (
+									<div className="rounded-sm border border-red-500 bg-red-100/80 px-3 py-3 flex flex-row items-center gap-2">
+										<AlertCircle className="size-5 stroke-red-500 stroke-2" />
+										<p className="text-sm text-red-500">
+											{registerMutation.error.message}
+										</p>
+									</div>
+								)}
+								<Field className="flex flex-col gap-3">
+									<Button
+										type="button"
+										onClick={handleRegister}
+										disabled={registerMutation.isPending}
+									>
+										{registerMutation.isPending ? (
+											<Loader2 className="size-4 animate-spin" />
+										) : null}
+										Register
+									</Button>
+									<FieldDescription className="text-center">
+										Already have an account?{" "}
+										<Link to={"/login"}>Sign in</Link>
+									</FieldDescription>
+								</Field>
+							</FieldGroup>
+						</CardContent>
+					</Card>
 				</div>
 			</div>
-		</div>
-	);
-}
-
-function InputField({
-	field,
-	placeholder,
-	value,
-	onChange,
-}: {
-	field: keyof RegisterForm;
-	placeholder: string;
-	value: RegisterForm;
-	onChange: (val: RegisterForm) => void;
-}) {
-	return (
-		<div className="flex flex-col gap-1">
-			<p className="text-sm font-semibold capitalize">{field}</p>
-			<Input
-				placeholder={placeholder}
-				value={value[field]}
-				onChange={(e) =>
-					onChange({
-						...value,
-						[field]: e.target.value,
-					})
-				}
-			/>
 		</div>
 	);
 }
--- frontend/src/pages/verify-email.tsx
+++ frontend/src/pages/verify-email.tsx
@@ -1,6 +1,12 @@
-import { useState } from "react";
 import { useAuth } from "@/components/auth-provider";
 import { Button } from "@/components/ui/button";
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardHeader,
+	CardTitle,
+} from "@/components/ui/card";
 import {
 	Field,
 	FieldDescription,
@@ -12,9 +18,9 @@ import {
 	InputOTPGroup,
 	InputOTPSlot,
 } from "@/components/ui/input-otp";
-import { cn } from "@/lib/utils";
 import { useMutation } from "@tanstack/react-query";
-import { AlertCircle } from "lucide-react";
+import { AlertCircle, GalleryVerticalEnd, Loader2 } from "lucide-react";
+import { useState } from "react";
 import { Navigate } from "react-router-dom";
 
 export function VerifyEmail() {
@@ -59,35 +65,40 @@ export function VerifyEmail() {
 	};
 
 	return (
-		<div className="flex h-dvh">
-			<div className="w-1/2 h-full bg-secondary"></div>
-			<div className="w-1/2 h-full flex">
-				<div className={cn("flex flex-col gap-6", "w-full m-auto")}>
-					<FieldGroup className="flex flex-col">
-						<div className="flex flex-col items-center gap-1 text-center">
-							<h1 className="text-2xl font-semibold">
-								Enter verification code
-							</h1>
-							<p className="text-muted-foreground text-sm text-balance">
-								We sent a 6-digit code to your email.
-							</p>
-						</div>
-
-						<div className="mx-auto w-auto flex flex-col gap-5">
-							<Field className="w-fit">
+		<div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+			<div className="flex w-full max-w-xs flex-col gap-6">
+				<a
+					href="#"
+					className="flex items-center gap-2 self-center font-medium"
+				>
+					<div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
+						<GalleryVerticalEnd className="size-4" />
+					</div>
+					Java Auth Example
+				</a>
+				<Card>
+					<CardHeader className="text-center">
+						<CardTitle className="text-xl">
+							Enter verification code
+						</CardTitle>
+						<CardDescription>
+							We sent a 6-digit code to your email.
+						</CardDescription>
+					</CardHeader>
+					<CardContent>
+						<FieldGroup>
+							<Field>
 								<FieldLabel htmlFor="otp" className="sr-only">
 									Verification code
 								</FieldLabel>
-
 								<InputOTP
-									id="otp"
 									maxLength={6}
+									id="otp"
 									required
-									className="min-w-fit"
 									value={otp}
-									onChange={setOtp} // <- controlled by React
+									onChange={setOtp}
 								>
-									<InputOTPGroup className="gap-2 *:data-[slot=input-otp-slot]:rounded-md *:data-[slot=input-otp-slot]:border min-w-fit">
+									<InputOTPGroup className="gap-2.5 *:data-[slot=input-otp-slot]:rounded-md *:data-[slot=input-otp-slot]:border">
 										<InputOTPSlot index={0} />
 										<InputOTPSlot index={1} />
 										<InputOTPSlot index={2} />
@@ -96,56 +107,46 @@ export function VerifyEmail() {
 										<InputOTPSlot index={5} />
 									</InputOTPGroup>
 								</InputOTP>
-
 								<FieldDescription className="text-center">
 									Enter the 6-digit code sent to your email.
 								</FieldDescription>
-
-								{verifyEmailMutation.isError && (
-									<div className="rounded-sm border border-red-500 bg-red-100/80 px-3 py-3 flex flex-row items-center gap-2">
-										<AlertCircle className="size-5 stroke-red-500 stroke-2" />
-										<p className="text-sm text-red-500">
-											{verifyEmailMutation.error.message}
-										</p>
-									</div>
-								)}
 							</Field>
-
-							<FieldGroup className="flex flex-col gap-2">
+							{verifyEmailMutation.isError && (
+								<div className="rounded-sm border border-red-500 bg-red-100/80 px-3 py-3 flex flex-row items-center gap-2">
+									<AlertCircle className="size-5 stroke-red-500 stroke-2" />
+									<p className="text-sm text-red-500">
+										{verifyEmailMutation.error.message}
+									</p>
+								</div>
+							)}
+							<Field className="flex flex-col gap-3">
 								<Button
-									type="button"
-									className="w-full"
 									onClick={handleVerify}
 									disabled={
-										otp.length !== 6 ||
-										verifyEmailMutation.isPending
+										verifyEmailMutation.isPending ||
+										otp.length < 6
 									}
 								>
-									{verifyEmailMutation.isPending
-										? "Verifying..."
-										: "Verify"}
+									{verifyEmailMutation.isPending ? (
+										<Loader2 className="size-4 animate-spin" />
+									) : null}
+									Verify
 								</Button>
-
-								<FieldDescription className="text-center flex flex-col gap-1">
+								<FieldDescription className="text-center">
 									{resendVerifEmail.isError ? (
 										<>
 											<span>
 												{resendVerifEmail.error.message}
 											</span>
-											<Button
-												type="button"
-												variant="outline"
-												size="sm"
+											<button
+												className="hover:text-black underline cursor-pointer"
 												onClick={handleResend}
-												disabled={
-													resendVerifEmail.isPending
-												}
 											>
 												Try again
-											</Button>
+											</button>
 										</>
 									) : resendVerifEmail.isPending ? (
-										<p>Sending email...</p>
+										<span>Sending email...</span>
 									) : (
 										<>
 											<span>
@@ -160,10 +161,10 @@ export function VerifyEmail() {
 										</>
 									)}
 								</FieldDescription>
-							</FieldGroup>
-						</div>
-					</FieldGroup>
-				</div>
+							</Field>
+						</FieldGroup>
+					</CardContent>
+				</Card>
 			</div>
 		</div>
 	);
