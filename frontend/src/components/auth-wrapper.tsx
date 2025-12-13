5da8327b979fa7b9e7704241592e54cbb6b1e2c8
rgpq25 <renzopq@hotmail.com>
2025-12-13 11:51:00 -0500

    feat(auth-wrapper): add responsive layout

--- frontend/src/components/auth-wrapper.tsx
+++ frontend/src/components/auth-wrapper.tsx
@@ -3,7 +3,7 @@ import type { PropsWithChildren } from "react";
 function AuthWrapper({ children }: PropsWithChildren) {
 	return (
 		<div className="bg-muted flex min-h-svh flex-row items-center justify-center">
-			<div className="flex w-1/2 bg-muted-foreground min-h-svh" />
+			<div className="hidden xl:flex xl:w-1/2 bg-muted-foreground min-h-svh" />
 			<div className="flex w-1/2 items-center justify-center">
 				{children}
 			</div>
